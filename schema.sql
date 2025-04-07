4b3e171e631ae23fb48e96382a1a480431c11d05
Brian Ludidi <bludidi5@gmail.com>
2025-04-07 10:45:06 +0200

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50) UNIQUE NOT NULL,
+  password VARCHAR(255) UNIQUE NOT NULL);
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(150) UNIQUE NOT NULL,
+  description TEXT NOT NULL,
+  due_date DATE,
+  status VARCHAR(20) DEFAULT 'Pending',
+  FOREIGN KEY (user_id) REFERENCES users(id));
